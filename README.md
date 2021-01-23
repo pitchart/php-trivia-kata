0363158ff272086bd6609f195780dd15e0b4454d
Julien VITTE <julien.vitte@insidegroup.fr>
2021-01-23 20:14:29 +0100

    chore: initializes repository

---
+++ README.md
@@ -0,0 +1,4 @@
+# Trivia refactor
+
+Just try to refactor the [Trivia Game](https://github.com/jbrains/trivia)
+
---
+++ bin/game-runner.php
@@ -0,0 +1,28 @@
+<?php
+
+require_once __DIR__.'/../vendor/autoload.php';
+
+use Trivia\Game;
+
+$notAWinner;
+
+$aGame = new Game();
+
+$aGame->add("Chet");
+$aGame->add("Pat");
+$aGame->add("Sue");
+
+
+do {
+
+    $aGame->roll(rand(0,5) + 1);
+
+    if (rand(0,9) == 7) {
+        $notAWinner = $aGame->wrongAnswer();
+    } else {
+        $notAWinner = $aGame->wasCorrectlyAnswered();
+    }
+
+
+
+} while ($notAWinner);
