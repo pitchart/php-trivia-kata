3265828b9f33614716e380be1d0aa27918bd2731
Julien VITTE <julien.vitte@insidegroup.fr>
2021-03-24 14:07:24 +0100

    feat(question): adds category management

--- src/Question.php
+++ src/Question.php
@@ -12,13 +12,26 @@ class Question
     private $label;
 
     /**
+     * @var string
+     */
+    private $category;
+
+
+    /**
      * Question constructor.
      *
      * @param string $label
      */
-    public function __construct(string $label)
+    public function __construct(string $label, string $category)
     {
+        if (!in_array($category, Category::all())) {
+            throw new \InvalidArgumentException(sprintf(
+                "Trying to create a question with invalid category name '%s', only values in [%s] are allowed",
+                $category, implode(', ', Category::all())
+            ));
+        }
         $this->label = $label;
+        $this->category = $category;
     }
 
     /**
@@ -29,4 +42,11 @@ class Question
         return $this->label;
     }
 
+    /**
+     * @return string
+     */
+    public function getCategory(): string
+    {
+        return $this->category;
+    }
 }
